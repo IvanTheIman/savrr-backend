a0c877faee9d5003f635667ce7fd2ddab37e2a60
IvanTheIman <imoran2487@gmail.com>
2026-04-20 11:32:31 -0500

    added comments to important functions

--- api/views/__init__.py
+++ api/views/__init__.py
@@ -1,8 +1,8 @@
-from views.barcode.barcode_add_view import add_barcode_to_product
-from views.barcode.barcode_lookup_view import barcode_lookup_view
-from views.grocery.grocery_list import grocery_lists
-from views.grocery.products import products_info
-from views.user_view import location_view, register_view, geocode_zipcode
+from .barcode.barcode_add_view import add_barcode_to_product
+from .barcode.barcode_lookup_view import barcode_lookup_view
+from .grocery.grocery_list import grocery_lists
+from .grocery.products import products_info
+from .user_view import location_view, register_view, geocode_zipcode
 
 __all__ = [
     'add_barcode_to_product',
